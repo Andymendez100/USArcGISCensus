3fcd77e263b75057d8e53b37fccf8bf1d4c10582
Andy <andymendez100@gmail.com>
2019-11-11 02:19:39 -0800

    Updates

---
+++ precache-manifest.cb135cdd5562fa461a1fc89edfc473ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3436ca06d638112b5643c3b88e98732d",
+    "url": "/USArcGISCensus/index.html"
+  },
+  {
+    "revision": "e325495aee6308162a86",
+    "url": "/USArcGISCensus/static/css/main.82e95e78.chunk.css"
+  },
+  {
+    "revision": "cd199a56a6ddcdae6db3",
+    "url": "/USArcGISCensus/static/js/2.bfef4a04.chunk.js"
+  },
+  {
+    "revision": "e325495aee6308162a86",
+    "url": "/USArcGISCensus/static/js/main.e08c593e.chunk.js"
+  },
+  {
+    "revision": "bf7dca0a5a100bf60c59",
+    "url": "/USArcGISCensus/static/js/runtime-main.c24607d3.js"
+  },
+  {
+    "revision": "a0795b65ed711ea23006691b7952a7e6",
+    "url": "/USArcGISCensus/static/media/US_Congressional.a0795b65.geojson"
+  },
+  {
+    "revision": "b20301c612917d6ae5296b4243ea7eec",
+    "url": "/USArcGISCensus/static/media/US_Counties.b20301c6.geojson"
+  },
+  {
+    "revision": "17fc7697c31731c7727f6b1bd70aa0d6",
+    "url": "/USArcGISCensus/static/media/US_Outline.17fc7697.geojson"
+  },
+  {
+    "revision": "8ec14844f3b39324ba4e381a947a53d9",
+    "url": "/USArcGISCensus/static/media/US_States.8ec14844.geojson"
+  }
+]);
